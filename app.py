0460c1ad69190f51267446867acc38a0010744d6
khaled <khaled.khazaee@gmail.com>
2025-12-15 08:53:40 +0100

    change health status to ok-v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/health")
 def health():
-    return jsonify(status="ok")
+    return jsonify(status="ok-v2")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
