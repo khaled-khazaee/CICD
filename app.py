a16dec49e102c0cedd58b659e6e12025f30e1abf
khaled <khaled.khazaee@gmail.com>
2025-12-15 11:10:39 +0100

    Change version to V4

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/health")
 def health():
-    return jsonify(status="ok-v2")
+    return jsonify(status="ok-v4")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
